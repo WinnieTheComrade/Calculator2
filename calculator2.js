f563e796280cb909949245914de1b747a061f404
Wachirawit <grosserludwig@gmail.com>
2023-03-02 00:06:47 +0700

    Link number btn to screen

--- calculator2.js
+++ calculator2.js
@@ -7,10 +7,23 @@ let operator = document.querySelectorAll('.operator');
 let clear = document.querySelector('.clear');
 let equal = document.querySelector('.equal');
 let decimal = document.querySelectorAll('.decimal');
+let buttons = document.querySelectorAll('.button')
+
+numbers.forEach((number) => number.addEventListener('click',function(e){
+    addNumber(e.target.textContent);
+    currentScreen.textContent = totalValue;
+
+    
+}))
+
+let addNumber = function(number){
+    totalValue += number
+}
+
 
 let operators = {
     add(operant1, operant2){
-    totalValue += operant1 + operant2;
+    console.log(operant1 + operant2)
     },
     subtract(operant1,operant2){
     totalValue -= operant1 - operant2;
@@ -26,5 +39,17 @@ let operators = {
 }
 
 let operate = function(operant1,operator,operant2){
-    if()
-}
+    if(operator === "+"){
+        operators.add(operant1,operant2);
+    }
+    else if (operator === "-"){
+        return operators.subtract(operant1,operant2);
+    }
+    else if (operator === "*"){
+        return operators.multiply(operant1,operant2);
+    }
+    else return operators.divide(operant1,operant2);
+    }
+
+operate(5,'+',8);
+
